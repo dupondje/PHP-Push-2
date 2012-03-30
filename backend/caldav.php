ae87dcc755f2dfabae0c2d554bda3b3d6e13f379
Jean-Louis Dupond <jean-louis@dupond.be>
2012-03-31 01:15:06 +0200

    Start parsing iCal to SyncObject

--- backend/caldav.php
+++ backend/caldav.php
@@ -170,11 +170,11 @@ class BackendCalDAV extends BackendDiff {
         
         if ($type == "calendar")
         {
-            return $this->_ParseVEventToEx($data);
+            return $this->_ParseVEventToEx($data, $contentparameters);
         }
         if ($type == "tasks")
         {
-            return $this->_ParseVTodoToEx($data);
+            return $this->_ParseVTodoToEx($data, $contentparameters);
         }
         return false;
     }
@@ -212,7 +212,8 @@ class BackendCalDAV extends BackendDiff {
         
         if ($id)
         {
-            $etag = $this->StatMessage($folderid, $id)['mod'];
+        	$mod = $this->StatMessage($folderid, $id);
+            $etag = $mod['mod'];
         }
         else
         {
@@ -244,7 +245,7 @@ class BackendCalDAV extends BackendDiff {
     public function DeleteMessage($folderid, $id)
     {
         ZLog::Write(LOGLEVEL_DEBUG, sprintf("BackendCalDAV->DeleteMessage('%s','%s')", $folderid,  $id));
-        $http_status_code = $this->cdc->DoDELETERequest($id);
+        $http_status_code = $this->_caldav->DoDELETERequest($id);
         if ($http_status_code == "204") {
             return true;
         }
@@ -259,24 +260,364 @@ class BackendCalDAV extends BackendDiff {
         return false;
     }
 
+    /*
+     * Convert a iCAL VEvent to ActiveSync format
+     */
+    private function _ParseVEventToAS($data, $contentparameters)
+    {
+    	$truncsize = Utils::GetTruncSize($contentparameters->GetTruncation());
+    	$message = new SyncAppointment();
+    	
+    	$ical = new iCalComponent($data);
+    	$timezones = $ical->GetComponents("VTIMEZONE");
+    	if (count($timezones) > 0)
+    	{
+    		$timezone = $timezones[0]->GetPValue("TZID");
+    		$message->timezone = $this->_GetTimezoneString($timezone);
+    	}
+    	
+    	$vevents = $ical->GetComponents("VTIMEZONE", false);
+    	foreach ($vevents as $event)
+    	{
+    		$reccurence_id = reset($event->GetProperties("RECURRENCE-ID"));
+    		if ($reccurence_id)
+    		{
+    			$exception = new SyncAppointmentException();
+    			$tzid = $reccurence_id->GetParameterValue("TZID");
+    			if (!$tzid)
+    			{
+    				$tzid = $timezone;
+    			}
+    			$exception->exceptionstarttime = $this->_MakeUTCDate($reccurence_id->Value(), $tzid);
+    			$exception->deleted = "0";
+    			$this->_ParseVEventToSyncObject($event, $exception, $truncsize);
+    			$message->exception[] = $exception;
+    		}
+    		else
+    		{
+    			$this->_ParseVEventToSyncObject($event, $message, $truncsize);
+    		}
+    	}
+    	return $message;
+    }
+    
+    private function _ParseVEventToSyncObject($event, &$message, $truncsize)
+    {
+    	$message = new SyncAppointment();
+    	$properties = $event->GetProperties();
+    	foreach ($properties as $property)
+    	{
+    		switch ($property->Name())
+    		{
+    			case "LAST-MODIFIED":
+    				$message->dtstamp = $this->_MakeUTCDate($property->Value());
+    				break;
+    				
+    			case "DTSTART":
+    				$message->starttime = $this->_MakeUTCDate($property->Value(), $property->GetParameterValue("TZID"));
+    				if (strlen($property->Value()) == 8)
+    				{
+    					$message->alldayevent = "1";
+    				}
+    				break;
+    				
+    			case "SUMMARY":
+    				$message->subject = $property->Value();
+    				break;
+    				
+    			case "UID":
+    				$message->uid = $property->Value();
+    				break;
+    				
+    			case "ORGANIZER":
+    				$org_str = str_replace(":MAILTO:", ";MAILTO=", $property->Value());
+    				$orgs = explode(";", $org_str);
+    				foreach ($orgs as $org)
+    				{
+    					$o = explode("=", $org);
+    					if ($o[0] == "CN")
+    					{
+    						$message->organizername = $o[1];
+    					}
+    					if ($o[0] == "MAILTO")
+    					{
+    						$message->organizeremail = $o[1];
+    					}
+    				}
+    				break;
+    				
+    			case "LOCATION":
+    				$message->location = $property->Value();
+    				break;
+
+    			case "DTEND":
+    				$message->endtime = $this->_MakeUTCDate($property->Value(), $property->GetParameterValue("TZID"));
+    				if (strlen($property->Value()) == 8)
+    				{
+    					$message->alldayevent = "1";
+    				}
+    				break;
+    				
+    			case "RRULE":
+    				$message->recurrence = $this->_ParseRecurrence($property->Value());
+    				break;
+
+    			case "CLASS":
+    				switch ($property->Value())
+    				{
+    					case "PUBLIC":
+    						$message->sensitivity = "0";
+    						break;
+    					case "PRIVATE":
+    						$message->sensitivity = "2";
+    						break;    						
+    					case "CONFIDENTIAL":
+    						$message->sensitivity = "3";
+    						break;
+    				}
+    				break;
+    				
+    			case "TRANSP":
+    				switch ($property->Value())
+    				{
+    					case "TRANSPARENT":
+    						$message->busystatus = "0";
+    						break;
+    					case "OPAQUE":
+    						$message->busystatus = "2";
+    						break;
+    				}
+    				break;
+    		}
+    	}
+    	
+    	$valarm = current($event->GetComponents("VALARM"));
+    	if ($valarm)
+    	{
+    		$properties = $event->GetProperties();
+    		foreach ($properties as $property)
+    		{
+    			switch ($property->Name())
+    			{
+    				case "TRIGGER":
+    					$val = $property->Value();
+    					$val = str_replace("-", "", $val);
+    					$interval = new DateInterval($val);
+    					$message->reminder = $interval->format("i");
+    					break;
+    			}
+    		}
+    	}
+    }
+    
+    private function _ParseRecurrence($rrules)
+    {
+    	$recurrence = new SyncRecurrence();
+    	$rrules = explode(";", $rrule);
+    	foreach ($rrules as $rrule)
+    	{
+    		$rule = explode("=", $rrule);
+    		switch ($rule[0])
+    		{
+    			case "FREQ":
+		    		switch ($rule[1])
+		    		{
+		    			case "DAILY":
+		    				$recurrence->type = "0";
+		    				break;
+		    			case "WEEKLY":
+		    				$recurrence->type = "1";
+		    				break;
+		    			case "MONTHLY":
+		    				$recurrence->type = "2";
+		    				break;
+		    			case "YEARLY":
+		    				$recurrence->type = "5";
+		    		}
+		    		break;
+		    		
+    			case "UNTIL":
+    				$recurrence->until = $this->_MakeUTCDate($rule[1]);
+    				break;
+    				
+    			case "COUNT":
+    				$recurrence->occurrences = $rule[1];
+    				break;
+    				
+    			case "INTERVAL":
+    				$recurrence->interval = $rule[1];
+    				break;
+
+    			case "BYDAY":
+    				$dval = 0;
+    				$days = explode(",", $rule[1]);
+    				foreach ($days as $day)
+    				{
+    					switch ($day)
+    					{
+    						//   1 = Sunday
+    						//   2 = Monday
+    						//   4 = Tuesday
+    						//   8 = Wednesday
+    						//  16 = Thursday
+    						//  32 = Friday
+    						//  62 = Weekdays  // not in spec: daily weekday recurrence
+    						//  64 = Saturday
+    						case "SU":
+    							$dval += 1;
+    							break;
+    						case "MO":
+    							$dval += 2;
+    							break;
+    						case "TU":
+    							$dval += 4;
+    							break;
+    						case "WE":
+    							$dval += 8;
+    							break;
+    						case "TH":
+    							$dval += 16;
+    							break;
+    						case "FR":
+    							$dval += 32;
+    							break;
+    						case "SA":
+    							$dval += 64;
+    							break;
+    					}
+    				}
+    				$recurrence->dayofweek = $dval;
+    				break;
+    				
+    			//Only 1 BYMONTHDAY is supported, so BYMONTHDAY=2,3 will only include 2
+    			case "BYMONTHDAY":
+    				$days = explode(",", $rule[1]);
+    				$recurrence->dayofmonth = $days[0];
+    				break;
+    			
+    			case "BYMONTH":
+    				$recurrence->monthofyear = $rule[1];
+    				break;
+    				
+    			default:
+    				ZLog::Write(LOGLEVEL_DEBUG, sprintf("BackendCalDAV->_ParseRecurrence(): '%s' is not yet supported.", $rule[0]));
+    		}
+    	}
+    }
+
     //TODO: Implement
-    private function _ParseVEventToEx($data)
+    private function _ParseASEventToVEvent($data)
     {
     }
 
     //TODO: Implement
-    private function _ParseExEventToVEvent($data)
+    private function _ParseVTodoToAS($data)
     {
     }
 
     //TODO: Implement
-    private function _ParseVTodoToEx($data)
+    private function _ParseASTaskToVTodo($data)
     {
     }
-
-    //TODO: Implement
-    private function _ParseExTaskToVTodo($data)
+    
+    private function _MakeUTCDate($value, $timezone = null)
     {
+    	if ($timezone)
+    	{
+    		$tz = timezone_open($timezone);
+    	}
+    	if (!$tz)
+    	{
+    		//If there is no timezone set, we use the default timezone
+    		$tz = timezone_open(date_default_timezone_get());
+    	}
+    	//20110930T090000Z
+    	date_create_from_format('Ymd\THis\Z', $time, timezone_open("UTC"));
+    	if (!$date)
+    	{
+    		//20110930T090000
+    		$date = date_create_from_format('Ymd\THis', $timestr, $tz);
+    	}
+    	if (!$date)
+    	{
+    		//20110930
+    		$date = date_create_from_format('Ymd', $timestr, $tz);
+    	}
+    	return date_timestamp_get($date);
+    }
+    
+    private function _GetTimezoneString($timezone, $with_names = true)
+    {
+    	// UTC needs special handling
+    	if ($timezone == "UTC")
+    		return base64_encode(pack('la64vvvvvvvvla64vvvvvvvvl', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0));
+    	try {
+    		//Generate a timezone string (PHP 5.3 needed for this)
+    		$timezone = new DateTimeZone($timezone);
+    		$trans = $timezone->getTransitions(time());
+    		$stdTime = null;
+    		$dstTime = null;
+    		if (count($trans) < 3)
+    		{
+    			throw new Exception();
+    		}
+    		if ($trans[1]['isdst'] == 1)
+    		{
+    			$dstTime = $trans[1];
+    			$stdTime = $trans[2];
+    		}
+    		else
+    		{
+    			$dstTime = $trans[2];
+    			$stdTime = $trans[1];
+    		}
+    		$stdTimeO = new DateTime($stdTime['time']);
+    		$stdFirst = new DateTime(sprintf("first sun of %s %s", $stdTimeO->format('F'), $stdTimeO->format('Y')));
+    		$stdInterval = $stdTimeO->diff($stdFirst);
+    		$stdDays = $stdInterval->format('%d');
+    		$stdBias = $stdTime['offset'] / -60;
+    		$stdName = $stdTime['abbr'];
+    		$stdYear = 0;
+    		$stdMonth = $stdTimeO->format('n');
+    		$stdWeek = floor($stdDays/7)+1;
+    		$stdDay = $stdDays%7;
+    		$stdHour = $stdTimeO->format('H');
+    		$stdMinute = $stdTimeO->format('i');
+    		$stdTimeO->add(new DateInterval('P7D'));
+    		if ($stdTimeO->format('n') != $stdMonth)
+    		{
+    			$stdWeek = 5;
+    		}
+    		$dstTimeO = new DateTime($dstTime['time']);
+    		$dstFirst = new DateTime(sprintf("first sun of %s %s", $dstTimeO->format('F'), $dstTimeO->format('Y')));
+    		$dstInterval = $dstTimeO->diff($dstFirst);
+    		$dstDays = $dstInterval->format('%d');
+    		$dstName = $dstTime['abbr'];
+    		$dstYear = 0;
+    		$dstMonth = $dstTimeO->format('n');
+    		$dstWeek = floor($dstDays/7)+1;
+    		$dstDay = $dstDays%7;
+    		$dstHour = $dstTimeO->format('H');
+    		$dstMinute = $dstTimeO->format('i');
+    		if ($dstTimeO->format('n') != $dstMonth)
+    		{
+    			$dstWeek = 5;
+    		}
+    		$dstBias = ($dstTime['offset'] - $stdTime['offset']) / -60;
+    		if ($with_names)
+    		{
+    			return base64_encode(pack('la64vvvvvvvvla64vvvvvvvvl', $stdBias, $stdName, 0, $stdMonth, $stdDay, $stdWeek, $stdHour, $stdMinute, 0, 0, 0, $dstName, 0, $dstMonth, $dstDay, $dstWeek, $dstHour, $dstMinute, 0, 0, $dstBias));
+    		}
+    		else
+    		{
+    			return base64_encode(pack('la64vvvvvvvvla64vvvvvvvvl', $stdBias, '', 0, $stdMonth, $stdDay, $stdWeek, $stdHour, $stdMinute, 0, 0, 0, '', 0, $dstMonth, $dstDay, $dstWeek, $dstHour, $dstMinute, 0, 0, $dstBias));
+    		}
+    	}
+    	catch (Exception $e) {
+    		// If invalid timezone is given, we return UTC
+    		return base64_encode(pack('la64vvvvvvvvla64vvvvvvvvl', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0));
+    	}
+    	return base64_encode(pack('la64vvvvvvvvla64vvvvvvvvl', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0));
     }
 }
 
